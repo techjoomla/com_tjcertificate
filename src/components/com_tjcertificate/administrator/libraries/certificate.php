055595ff494e3ee5dc69e5d2f41208e7808f20c2
Praneet <praneet_s@tekditechnologies.com>
2020-11-20 10:05:01 +0530

    Issue #166434 task: Server side certificate image generation using Imagick

--- src/components/com_tjcertificate/administrator/libraries/certificate.php
+++ src/components/com_tjcertificate/administrator/libraries/certificate.php
@@ -659,7 +659,7 @@ class TjCertificateCertificate extends CMSObject
 	 *
 	 * @param   integer  $store  Store as attachment for emails
 	 *
-	 * @return  integer|string Certificate pdf url.
+	 * @return  boolean|string Certificate pdf url.
 	 *
 	 * @since 1.0
 	 */
