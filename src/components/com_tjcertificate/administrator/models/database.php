02da2b751d048d18946ebb45cbe14d4eeb780721
Divya Chaudhari <divya_c@techjoomla.com>
2021-12-03 09:34:26 +0530

    Task #173952 Chore: Including Joomla v3.10 and Joomla v4.0 compatiblity

--- src/components/com_tjcertificate/administrator/models/database.php
+++ src/components/com_tjcertificate/administrator/models/database.php
@@ -12,122 +12,70 @@
 defined('_JEXEC') or die('Restricted access');
 use Joomla\CMS\Table\Table;
 use Joomla\CMS\Factory;
-
+use Joomla\CMS\MVC\Model\BaseDatabaseModel;
 
 if (JVERSION < '4.0.0')
 {
-	require_once JPATH_ADMINISTRATOR . '/components/com_installer/src/Model/DatabaseModel.php';
-    require_once JPATH_ADMINISTRATOR . '/components/com_config/src/Model/ApplicationModel.php';
+	require_once JPATH_ADMINISTRATOR . '/components/com_installer/models/database.php';
+    require_once JPATH_ADMINISTRATOR . '/components/com_config/models/application.php';
+    require_once JPATH_SITE . '/components/com_config/model/cms.php';
+	require_once JPATH_SITE . '/components/com_config/model/form.php';
+}
+
+/**
+* Manage TjCertificate database operations
+*
+* @since  1.0.0
+*/
+class TjCertificateModelDatabase extends BaseDatabaseModel
+{
+	protected $extensionName = 'com_tjcertificate';
 
 	/**
-	 * Manage TjCertificate database operations
+	 * Function used to set permission as allowed for manager user groups if not set to any other
 	 *
-	 * @since  1.0.0
+	 * @return  void
+	 *
+	 * @since   1.0
 	 */
-	class TjCertificateModelDatabase extends InstallerModelDatabase
+	public function allowEditOwnForManagerGroup()
 	{
-		protected $extensionName = 'com_tjcertificate';
+		$db = Factory::getDbo();
 
-		/**
-		 * Function used to set permission as allowed for manager user groups if not set to any other
-		 *
-		 * @return  void
-		 *
-		 * @since   1.0
-		 */
-		public function allowEditOwnForManagerGroup()
+		$templateEditOwn = 'template.edit.own';
+
+		// Get order Ids of selected course
+		$query = $db->getQuery(true);
+		$query->select('rules');
+		$query->from('#__assets');
+		$query->where($db->quoteName('name') . ' = ' . $db->quote($this->extensionName));
+		$db->setQuery($query);
+		$temp = $db->loadResult();
+
+		$rules = (array) json_decode($temp);
+
+		if (empty($rules) || !in_array($templateEditOwn, $rules))
 		{
-			$db = Factory::getDbo();
+			$managerGroup = Table::getInstance('Usergroup', 'JTable');
+			$managerGroup->load(array('title' => 'Manager'));
 
-			$templateEditOwn = 'template.edit.own';
+			// Get Post DATA
+			$permissions = array(
+			'component' => $this->extensionName,
+			'action'    => $templateEditOwn,
+			'rule'      => $managerGroup->id,
+			'value'     => '1',
+			'title'     => $this->extensionName
+									);
 
-			// Get order Ids of selected course
-			$query = $db->getQuery(true);
-			$query->select('rules');
-			$query->from('#__assets');
-			$query->where($db->quoteName('name') . ' = ' . $db->quote($this->extensionName));
-			$db->setQuery($query);
-			$temp = $db->loadResult();
-
-			$rules = (array) json_decode($temp);
-
-			if (empty($rules) || !in_array($templateEditOwn, $rules))
+			if (JVERSION < 4.0)
 			{
-				$managerGroup = Table::getInstance('Usergroup', 'JTable');
-				$managerGroup->load(array('title' => 'Manager'));
-
-				require_once JPATH_SITE . '/components/com_config/model/cms.php';
-				require_once JPATH_SITE . '/components/com_config/model/form.php';
-				require_once JPATH_ADMINISTRATOR . '/components/com_config/models/application.php';
-
-				// Get Post DATA
-				$permissions = array(
-					'component' => $this->extensionName,
-					'action'    => $templateEditOwn,
-					'rule'      => $managerGroup->id,
-					'value'     => '1',
-					'title'     => $this->extensionName
-				);
-
 				// Load Permissions from Session and send to Model
 				$model    = new ConfigModelApplication;
 				$response = $model->storePermissions($permissions);
 			}
-		}
-	}
-}
-else
-{
-	/**
-	 * Manage TjCertificate database operations
-	 *
-	 * @since  1.0.0
-	 */
-	class TjCertificateModelDatabase extends Joomla\Component\Installer\Administrator\Model\DatabaseModel
-	{
-		protected $extensionName = 'com_tjcertificate';
-
-		/**
-		 * Function used to set permission as allowed for manager user groups if not set to any other
-		 *
-		 * @return  void
-		 *
-		 * @since   1.0
-		 */
-		public function allowEditOwnForManagerGroup()
-		{
-			$db = Factory::getDbo();
-
-			$templateEditOwn = 'template.edit.own';
-
-			// Get order Ids of selected course
-			$query = $db->getQuery(true);
-			$query->select('rules');
-			$query->from('#__assets');
-			$query->where($db->quoteName('name') . ' = ' . $db->quote($this->extensionName));
-			$db->setQuery($query);
-			$temp = $db->loadResult();
-
-			$rules = (array) json_decode($temp);
-
-			if (empty($rules) || !in_array($templateEditOwn, $rules))
+			else
 			{
-				$managerGroup = Table::getInstance('Usergroup', 'JTable');
-				$managerGroup->load(array('title' => 'Manager'));
-
-				// require_once JPATH_SITE . '/components/com_config/model/cms.php';
-				// require_once JPATH_SITE . '/components/com_config/model/form.php';
-				// require_once JPATH_ADMINISTRATOR . '/components/com_config/models/application.php';
-
-				// Get Post DATA
-				$permissions = array(
-					'component' => $this->extensionName,
-					'action'    => $templateEditOwn,
-					'rule'      => $managerGroup->id,
-					'value'     => '1',
-					'title'     => $this->extensionName
-				);
-
 				// Load Permissions from Session and send to Model
 				$model    = new Joomla\Component\Config\Administrator\Model\ApplicationModel;
 				$response = $model->storePermissions($permissions);
