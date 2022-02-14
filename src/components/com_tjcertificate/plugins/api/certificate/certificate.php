f65fb743bfa8ce611a64d0aaa1eb64c72d773d70
Megha Biranje <megha_b@techjoomla.com>
2022-02-14 15:59:27 +0530

    Task #179500 Fix: TJCertificate => add plugins and install it while installing the pkg

---
+++ src/components/com_tjcertificate/plugins/api/certificate/certificate.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @package     TJ.Certificate
+ * @subpackage  Api.TjCertificate
+ *
+ * @copyright   Copyright (C) 2009 - 2022 Techjoomla. All rights reserved.
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+
+defined('_JEXEC') or die( 'Restricted access' );
+
+jimport('joomla.plugin.plugin');
+
+/**
+ * Plugin API TjCertificate
+ *
+ * @since  1.0.0
+ */
+class PlgAPICertificate extends ApiPlugin
+{
+	/**
+	 * Constructor.
+	 *
+	 * @param   object  &$subject  The object to observe
+	 * @param   array   $config    An optional associative array of configuration settings.
+	 *
+	 * @since   3.7.0
+	 */
+	public function __construct(&$subject, $config = array())
+	{
+		parent::__construct($subject, $config = array());
+
+		ApiResource::addIncludePath(dirname(__FILE__) . '/certificate');
+
+		/*load language file for plugin frontend*/
+		$lang = JFactory::getLanguage();
+		$lang->load('plg_api_certificate', JPATH_ADMINISTRATOR,'',true);
+
+		// Set the login resource to be public
+		$this->setResourceAccess('download', 'public', 'get');
+	}
+}
--- src/components/com_tjcertificate/script.tjcertificate.php
+++ src/components/com_tjcertificate/script.tjcertificate.php
@@ -23,6 +23,17 @@ jimport('joomla.filesystem.file');
  */
 class Com_TjcertificateInstallerScript
 {
+	/** @var array The list of extra modules and plugins to install */
+	private $installation_queue = array(
+
+		// Plugins => { (folder) => { (element) => (published) }* }*
+		'plugins' => array(
+				'api' => array(
+					'certificate' => 0,
+				)
+			)
+		);
+
 	/**
 	 * Runs after install, update or discover_update
 	 *
@@ -46,6 +57,73 @@ class Com_TjcertificateInstallerScript
 			return false;
 		}
 
+		// Install subextensions
+		$this->_installSubextensions($parent);
+
 		return true;
+	}
+
+	/**
+	 * Installs subextensions (modules, plugins) bundled with the main extension
+	 *
+	 * @param   JInstaller $parent
+	 *
+	 * @return JObject The subextension installation status
+	 */
+	private function _installSubextensions($parent)
+	{
+		$src = $parent->getParent()->getPath('source');
+
+		$db = JFactory::getDbo();
+
+		// Plugins installation
+		if (count($this->installation_queue['plugins']))
+		{
+			foreach ($this->installation_queue['plugins'] as $folder => $plugins)
+			{
+				if (count($plugins))
+				{
+					foreach ($plugins as $plugin => $published)
+					{
+						$path = "$src/plugins/$folder/$plugin";
+
+						if (!is_dir($path))
+						{
+							$path = "$src/plugins/$folder/plg_$plugin";
+						}
+
+						if (!is_dir($path))
+						{
+							$path = "$src/plugins/$plugin";
+						}
+
+						if (!is_dir($path))
+						{
+							$path = "$src/plugins/plg_$plugin";
+						}
+
+						if (!is_dir($path))
+						{
+							continue;
+						}
+
+						// Was the plugin already installed?
+						$query = $db->getQuery(true)->select('COUNT(*)')->from($db->qn('#__extensions'))->where($db->qn('element') . ' = ' . $db->q($plugin))->where($db->qn('folder') . ' = ' . $db->q($folder));
+						$db->setQuery($query);
+						$count = $db->loadResult();
+
+						$installer = new JInstaller;
+						$result    = $installer->install($path);
+
+						if ($published && !$count)
+						{
+							$query = $db->getQuery(true)->update($db->qn('#__extensions'))->set($db->qn('enabled') . ' = ' . $db->q('1'))->where($db->qn('element') . ' = ' . $db->q($plugin))->where($db->qn('folder') . ' = ' . $db->q($folder));
+							$db->setQuery($query);
+							$db->execute();
+						}
+					}
+				}
+			}
+		}
 	}
 }
